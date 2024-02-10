08bdbef678055a496486000b2d6b0baa98778e58
Viktor <101078564+JojokerFights@users.noreply.github.com>
2024-02-10 09:04:50 +0200

    Update script.js

--- jone/script.js
+++ jone/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const slider = document.getElementById("myRange");
     const output = document.getElementById("demo");
     const listContainer = document.getElementById("listContainer");
-    output.textContent = slider.value; // Используем textContent вместо innerHTML
+    output.textContent = slider.value;
 
     function updateList() {
         while (listContainer.firstChild) {
@@ -40,8 +40,8 @@ document.addEventListener('DOMContentLoaded', function () {
     function updateImage(index, imgElement) {
         // Имитация задержки перед выполнением запроса
         delay(2000) // Задержка в 2000 мс (2 секунды)
-        .then(() => fetch('https://api.thecatapi.com/v1/images/search?size=small', {
-            headers: { 'x-api-key': 'api_key=live_xuBbWHlLwy2BIScAAxftB9fBwsIZtBl268TFLf3dMtE5RZcq1cntWIzyzLmjUDQI' } // Замените на ваш ключ API
+        .then(() => fetch('https://api.thecatapi.com/v1/images/search?size=small',{
+        headers: { 'x-api-key':'api_key=live_xuBbWHlLwy2BIScAAxftB9fBwsIZtBl268TFLf3dMtE5RZcq1cntWIzyzLmjUDQI' } // Замените на ваш ключ API
         }))
         .then(response => response.json())
         .then(data => {
